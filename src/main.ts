ae651455f703991f8455268a8d8fde3c0682d9ff
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2023-06-16 00:05:26 +0300

    fix CORS

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3001',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  };
-  app.enableCors(corsOptions)
+  const app = await NestFactory.create(AppModule, {cors: false});
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   await app.listen(3001);
 }
