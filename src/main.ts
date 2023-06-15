ba22089a82429f9d5e169233d03b0ce3bf9b3ed6
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2023-06-15 23:57:59 +0300

    bug fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const corsOptions: CorsOptions = {
-    origin: 'http://lokalhost:3001',
+    origin: 'http://localhost:3001',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   };
