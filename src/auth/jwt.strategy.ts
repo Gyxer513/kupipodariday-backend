8d1629f6f6b515342e3f04c810baebcf58256afe
Gyxer513 <fpanchuk@yandex.ru>
2023-06-16 15:40:30 +0300

    fix auth after review

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,20 +1,31 @@
 import { ExtractJwt } from 'passport-jwt';
 import { Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(configService: ConfigService) {
+  constructor(
+    configService: ConfigService,
+    private usersService: UsersService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: configService.get<string>('jwt_secret'),
       ignoreExpiration: false,
     });
   }
-  async validate(payload: any) {
-    return { userId: payload.sub, username: payload.username };
+  async validate(jwtPayload: { sub: number }) {
+    const user = await this.usersService.findOne({
+      where: { id: jwtPayload.sub },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Необходима авторизация');
+    }
+
+    return user;
   }
 }
