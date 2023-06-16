3d8c669b707894f27af71f49e66be8cf8b2d4e30
Gyxer513 <fpanchuk@yandex.ru>
2023-06-16 15:54:38 +0300

    fix wishlistslists

--- src/wishlists/wishlists.controller.ts
+++ src/wishlists/wishlists.controller.ts
@@ -16,7 +16,7 @@ import { CustomRequest } from '../utils/custom-request'
 import { JwtGuard } from 'src/auth/jwt.guard';
 
 @UseGuards(JwtGuard)
-@Controller('wishlists')
+@Controller('wishlistlists')
 export class WishlistsController {
   constructor(private readonly wishlistsService: WishlistsService) { }
 
